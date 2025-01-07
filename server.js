0ed7f21db7fa407c71827b19c4b9ab77d1fbbcd2
rexx <alinco2319@gmail.com>
2025-01-07 10:49:52 +0100

    like

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/" , (req , res)=>{
+    res.send("Hello World");
+})
+
 app.listen(port , ()  => {
     console.log(`Server is running on port ${port}`);
 })
